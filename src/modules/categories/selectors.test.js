e7e08233f2659e034c4c2c4c331271c255b35f8e
Thomas Hourlier <hourliert@gmail.com>
2017-10-02 08:09:25 +0100

    test(*.selectors): tests selectors

--- src/modules/categories/selectors.test.js
+++ src/modules/categories/selectors.test.js
@@ -1,7 +1,65 @@
-import * as categoriesSelectors from './selectors';
+/**
+ * This test is a copy of ../products/selectors.test.js
+ * If I had more time I would have use this library https://github.com/redux-observable/redux-observable
+ * that is an abstraction of crud action creators. This library is written by me an fully tested.
+ */
 
-describe('categoriesSelectors', () => {
-  it('passes', () => {
-    expect(true).toBe(true);
+import { fromJS } from 'immutable';
+
+import * as categorySelectors from './selectors';
+
+const initialRawState = {
+  value: {
+    '1234': { id: '1234', title: 'Liquid' },
+    '4567': { id: '4567', title: 'Sauce' },
+  },
+  selected: '1234',
+};
+
+function getInitialState() {
+  return {
+    categories: fromJS(initialRawState),
+  };
+}
+
+describe('categorySelectors', () => {
+  it('retrieves the categories value', () => {
+    const state = getInitialState();
+
+    const value = categorySelectors.getCategoriesValue(state);
+
+    expect(value.toJS()).toEqual(initialRawState.value);
+  });
+
+  it('retrieves the categories value as a list', () => {
+    const state = getInitialState();
+
+    const value = categorySelectors.getCategoriesValueList(state);
+
+    expect(value.toJS()).toEqual(Object.values(initialRawState.value));
+  });
+
+  it('retrieves the selected category id', () => {
+    const state = getInitialState();
+
+    const id = categorySelectors.getSelectedCategoryId(state);
+
+    expect(id).toEqual(initialRawState.selected);
+  });
+
+  it('retrieves the selected category', () => {
+    const state = getInitialState();
+
+    const category = categorySelectors.getSelectedCategoryValue(state);
+
+    expect(category.toJS()).toEqual(initialRawState.value[initialRawState.selected]);
+  });
+
+  it('retrieves the selected category as raw JS', () => {
+    const state = getInitialState();
+
+    const category = categorySelectors.getSelectedCategory(state);
+
+    expect(category).toEqual(initialRawState.value[initialRawState.selected]);
   });
 });
--- src/modules/products/selectors.test.js
+++ src/modules/products/selectors.test.js
@@ -1,7 +1,97 @@
+import { fromJS } from 'immutable';
+
 import * as productSelectors from './selectors';
 
+jest.mock('../categories', () => ({
+  getSelectedCategoryId: jest.fn(() => '2'),
+}));
+jest.mock('redux-form/immutable', () => ({
+  getFormValues: jest.fn(() => jest.fn(() => {
+    const Immutable = require('immutable');
+
+    return Immutable.fromJS({
+      search: 'May',
+    });
+  })),
+}));
+
+const initialRawState = {
+  value: {
+    '1234': { id: '1234', title: 'Soup', description: '', categories: [{ id: '1', title: 'Liquid'}] },
+    '4567': { id: '4567', title: 'Ketchup', description: 'May be good', categories: [{ id: '2', title: 'Sauce'}] },
+    '6789': { id: '6789', title: 'Mayo', description: '', categories: [{ id: '2', title: 'Sauce'}] },
+    '9023': { id: '9023', title: 'Mustard', description: '', categories: [{ id: '2', title: 'Sauce'}] },
+  },
+  selected: '1234',
+};
+
+function getInitialState() {
+  return {
+    products: fromJS(initialRawState),
+  };
+}
+
 describe('productSelectors', () => {
-  it('passes', () => {
-    expect(true).toBe(true);
+  it('retrieves the products value', () => {
+    const state = getInitialState();
+
+    const value = productSelectors.getProductsValue(state);
+
+    expect(value.toJS()).toEqual(initialRawState.value);
+  });
+
+  it('retrieves the products value as a list', () => {
+    const state = getInitialState();
+
+    const value = productSelectors.getProductsValueList(state);
+
+    expect(value.toJS()).toEqual(Object.values(initialRawState.value));
+  });
+
+  it('retrieves the selected product id', () => {
+    const state = getInitialState();
+
+    const id = productSelectors.getSelectedProductId(state);
+
+    expect(id).toEqual(initialRawState.selected);
+  });
+
+  it('retrieves the selected product', () => {
+    const state = getInitialState();
+
+    const product = productSelectors.getSelectedProductValue(state);
+
+    expect(product.toJS()).toEqual(initialRawState.value[initialRawState.selected]);
+  });
+
+  it('retrieves the selected product as raw JS', () => {
+    const state = getInitialState();
+
+    const product = productSelectors.getSelectedProduct(state);
+
+    expect(product).toEqual(initialRawState.value[initialRawState.selected]);
+  });
+
+  it('retrieves the products for the selected category (id: 2)', () => {
+    const state = getInitialState();
+
+    const products = productSelectors.getSelectedCategoryProductsValue(state);
+
+    expect(products.toJS()).toEqual({
+      '4567': initialRawState.value['4567'],
+      '6789': initialRawState.value['6789'],
+      '9023': initialRawState.value['9023'],
+    });
+  });
+
+  it('retrieves the filtered products', () => {
+    const state = getInitialState();
+
+    const products = productSelectors.getFilteredProductsValue(state);
+
+    expect(products.toJS()).toEqual({
+      '4567': initialRawState.value['4567'],
+      '6789': initialRawState.value['6789'],
+    });
   });
 });
